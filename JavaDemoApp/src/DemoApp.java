9c96b73a352dc1f4934fb08e26d764047e541bf1
Ashok <ashokmaharana000@gmail.com>
2024-04-27 15:39:47 +0530

    Main Branch

--- JavaDemoApp/src/DemoApp.java
+++ JavaDemoApp/src/DemoApp.java
@@ -3,7 +3,7 @@ public class DemoApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.print("Branch 1");
+		System.out.print("Main Branch");
 	}
 
 }
